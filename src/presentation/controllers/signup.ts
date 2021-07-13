0a7db401a18a152c78306e27d647f6000e31b8c2
Gabriel <gabrielmvelezmoro592@gmail.com>
2021-07-13 16:22:42 -0300

    refactor: mode server error to a helper methd

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,10 +1,9 @@
 import { MissingParamError } from '../errors/missing-param-error'
 import {HttpResponse, HttpRequest} from '../protocols/http'
-import {badRequest} from '../helpers/http-helper'
+import {badRequest, serverError} from '../helpers/http-helper'
 import {Controller} from '../protocols/controller'
 import {EmailValidator} from '../protocols/email-validator'
 import {InvalidParamError} from '../errors/invalid-param-error'
-import { ServerError } from '../errors/server-error'
 
 
 export class SignUpController implements Controller {
@@ -13,8 +12,9 @@ export class SignUpController implements Controller {
     constructor (emailValidator: EmailValidator){
         this.emailValidator = emailValidator
     }
+    
     handle (httpRequest: HttpRequest): HttpResponse {
-       try { const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
+    try { const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
         for (const field of requiredFields) {
             if (!httpRequest.body[field]){
                 return badRequest ( new MissingParamError(field))
@@ -25,10 +25,7 @@ export class SignUpController implements Controller {
             return badRequest ( new InvalidParamError('email')) 
         }
     } catch (error) {
-        return {
-            statusCode: 500,
-            body: new ServerError()
-        }
+        return serverError()
     }
     }
 }
