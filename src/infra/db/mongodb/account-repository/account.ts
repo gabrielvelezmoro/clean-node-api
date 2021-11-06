e46d1e6ffc0fce8cdbe529910fbcdfc19a337238
Gabriel <gabrielmvelezmoro592@gmail.com>
2021-11-06 10:49:41 -0300

    feat: ensure AccountMongoRepository returns an account on asuccess

---
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -0,0 +1,21 @@
+import { AddAccountRepository } from '../../../../data/protocols/add-account-repository'
+import { AccountModel } from '../../../../domain/models/account'
+import { AddAccountModel } from '../../../../domain/usecases/add-account'
+import { MongoHelper } from '../helpers/mongo-helper'
+
+export class AccountMongoRepository implements AddAccountRepository {
+  async add (accountData: AddAccountModel): Promise<AccountModel> {
+    const accountCollection = MongoHelper.getCollecton('accounts')
+    const result = await accountCollection.insertOne(accountData)
+    const id = result.insertedId
+    const returnedAccount = await accountCollection.findOne({ _id: id })
+
+    const account: AccountModel = {
+      id: returnedAccount._id,
+      name: returnedAccount.name,
+      email: returnedAccount.email,
+      password: returnedAccount.password
+    }
+    return account
+  }
+}
